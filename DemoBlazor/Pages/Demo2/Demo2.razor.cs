7e373adf1eb59879bc5d28a2bb27a93d7fad6fe4
stevebstorm <steve.lorent@gmail.Com>
2023-11-14 10:22:17 +0100

    Ajout démo 2 Component

---
+++ DemoBlazor/Pages/Demo2/Demo2.razor.cs
@@ -0,0 +1,12 @@
+﻿namespace DemoBlazor.Pages.Demo2
+{
+    public partial class Demo2
+    {
+        public int InfoFromChildren { get; set; }
+
+        void ReceiveInfo(int i)
+        {
+            InfoFromChildren = i;
+        }
+    }
+}
---
+++ DemoBlazor/Pages/Demo2/Demo2Bis.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace DemoBlazor.Pages.Demo2
+{
+    public partial class Demo2Bis
+    {
+        [Parameter]
+        public int ValueFromParent { get; set; }
+        [Parameter]
+        public EventCallback<int> MonSuperEvent { get; set; }
+        public  int Value { get; set; }
+        void SendToPapa() {
+            MonSuperEvent.InvokeAsync(Value);
+        }
+    }
+}
