bc907633daafabd897f425bf1e118a5126a01d57
stevebstorm <steve.lorent@gmail.Com>
2023-11-14 16:14:02 +0100

    Correction Exo quizz

---
+++ DemoBlazor/Pages/Exos/Exo1/Question.razor.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace DemoBlazor.Pages.Exos.Exo1
+{
+    public partial class Question
+    {
+        [Parameter]
+        public string Pseudo { get; set; }
+
+        [Parameter]
+        public EventCallback<string> NotifyResponse { get; set; }
+        [Parameter]
+        public EventCallback NotifyGameIsOver { get; set; }
+
+        public List<string> QuestionList{ get; set; }
+        public int Counter { get; set; }
+        public string CurrentQuestion { get; set; }
+
+        public Question()
+        {
+            QuestionList = new List<string>();
+            QuestionList.Add("Avez-vous bien mangé à midi ?");
+            QuestionList.Add("Avez vous compris Blazor ?");
+            QuestionList.Add("Une tite pause ?");
+
+            Counter = 0;
+            CurrentQuestion = QuestionList[Counter];
+            Counter++;
+
+        }
+
+        void Repondre(string resp)
+        {
+            NotifyResponse.InvokeAsync(resp);
+            
+            if(Counter >= QuestionList.Count)
+            {
+                CurrentQuestion = "";
+                NotifyGameIsOver.InvokeAsync();
+            }
+            else
+            {
+                CurrentQuestion = QuestionList[Counter];
+                Counter++;
+            }
+        }
+    }
+}
---
+++ DemoBlazor/Pages/Exos/Exo1/Quizz.razor.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection.PortableExecutable;
+
+namespace DemoBlazor.Pages.Exos.Exo1
+{
+    public partial class Quizz
+    {
+        public string Pseudo { get; set; }
+
+        public List<string> Responses { get; set; }
+        public bool IsFinished { get; set; } = false;
+        public Quizz()
+        {
+            Responses = new List<string>();
+        }
+
+        void AddResponse(string r)
+        {
+            Responses.Add(r);
+        }
+
+        void FinishGame()
+        {
+            IsFinished = true;
+        }
+    }
+}
