1d7541fcebe380ee05ee6da12d321103447395ad
steve <steve.lorent@bstorm.be>
2023-11-15 17:00:08 +0100

    Demo JsRuntime + Inject

---
+++ DemoBlazor/Pages/Demo6/Demo6.razor.cs
@@ -0,0 +1,26 @@
+﻿using DemoBlazor.Pages.Exos.Exo2.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace DemoBlazor.Pages.Demo6
+{
+    public partial class Demo6
+    {
+        [Inject]
+        public IJSRuntime _js { get; set; }
+
+        public string MyValue { get; set; }
+
+        async void SaveValue()
+        {
+            await _js.InvokeVoidAsync("localStorage.setItem", "valeur", MyValue);
+            
+        }
+        async void ReadValue()
+        {
+            string s = await _js.InvokeAsync<string>("localStorage.getItem", "valeur");
+            await Console.Out.WriteLineAsync(s);
+        }
+
+    }
+}
