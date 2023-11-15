77f78764857b41166ce1ff57725585398ab14314
steve <steve.lorent@bstorm.be>
2023-11-15 11:18:18 +0100

    Demo Lifecycle +virtualize

---
+++ DemoBlazor/Pages/Demo4/Demo4.razor.cs
@@ -0,0 +1,16 @@
+﻿namespace DemoBlazor.Pages.Demo4
+{
+    public partial class Demo4
+    {
+        List<Guid> guids { get; set; }
+
+        public Demo4()
+        {
+            guids = new List<Guid>();
+            for(int i = 0; i < 500; i++)
+            {
+                guids.Add(Guid.NewGuid());
+            }
+        }
+    }
+}
---
+++ DemoBlazor/Pages/Demo5/Demo5.razor.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace DemoBlazor.Pages.Demo5
+{
+    public partial class Demo5
+    {
+        public int Counter { get; set; }
+
+        //Etape 1
+        public Demo5()
+        {
+            Console.WriteLine("Passage dans le constructeur");
+        }
+
+
+        //Etape 2
+        protected override async Task OnInitializedAsync()
+        {
+            Console.WriteLine("Passage dans OnInitialized");
+        }
+
+        //Etape 3 ==> On charge les composants Enfants (Demo5Bis)
+
+        //Etape 4 ==> AfterRender prend la main
+        //Etape 4bis ==> Chaque changement d'une valeur passée en paramètre dans l'enfant
+        //               Déclenche OnParameterSet (se trouvant dans l'enfant)
+        protected override void OnAfterRender(bool firstRender)
+        {
+            if (firstRender)
+            {
+                Console.WriteLine("Nombre de rendu : " + Counter);
+                Console.WriteLine("First render");
+            }
+            else
+               
+                Console.WriteLine("La catégorie recherchée porte l'id : " + Counter);
+
+        }
+
+        public void SimulateRender()
+        {
+            Counter++;
+        }
+
+    }
+}
